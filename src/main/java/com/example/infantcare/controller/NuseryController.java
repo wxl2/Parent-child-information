32414aa3859a80c5825b1f8326fa43817c36074b
wxl2 <1263311820@qq.com>
2021-09-13 17:04:15 +0800

    change for upload imager with nusery manager

--- src/main/java/com/example/infantcare/controller/NuseryController.java
+++ src/main/java/com/example/infantcare/controller/NuseryController.java
@@ -1,15 +1,17 @@
 package com.example.infantcare.controller;
 
-import com.example.infantcare.pojo.ClientInfo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.example.infantcare.pojo.Nusery;
-import com.example.infantcare.service.ClientInfoService;
 import com.example.infantcare.service.NuseryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,22 +21,37 @@ public class NuseryController {
     @Autowired
     NuseryService nuseryService;
     @PostMapping("/addNusery")
-    public String addNusery(@RequestBody Map<String,Object> map){
-        String name =( String) map.get("name");
-        String imageurl =(String) map.get("imageurl");
-        String age =( String) map.get("age");
-        String constellation =( String) map.get("constellation");
-        String edulevel =( String) map.get("edulevel");
-        String marry =( String) map.get("marry");
-        String credential =( String) map.get("credential");
-        String level =( String) map.get("level");
-        String reason =( String) map.get("reason");
-        String work =( String) map.get("work");
+    public String addNusery(@RequestParam("file") MultipartFile file, @RequestParam("params")String params){
+        Map<String,Object> map = new HashMap<String,Object>();
+        JSONObject obj = JSON.parseObject(params);
+        String name = obj.getString("name");
+        String imageurl = obj.getString("imageurl");
+        int age = obj.getInteger("age");
+        String constellation = obj.getString("constellation");
+        String edulevel = obj.getString("edulevel");
+        String marry = obj.getString("marry");
+        String credential = obj.getString("credential");
+        int level = obj.getInteger("level");
+        String reason = obj.getString("reason");
+        String work = obj.getString("work");
 
-        Nusery nusery = new Nusery(0,name,imageurl,Integer.parseInt(age),constellation,edulevel,
-                marry,credential,Integer.parseInt(level),reason,work);
+        Nusery nusery = new Nusery(0,name,imageurl,age,constellation,edulevel,
+                marry,credential,level,reason,work);
 
-        if(nuseryService.addNusery(nusery) < 0){
+
+        try{
+            Date now = new Date();
+            SimpleDateFormat f=new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-");
+            String path = ResourceUtils.getURL("classpath:").getPath()+"static/images/head";
+            String fileName = f.format(now)+file.getOriginalFilename();
+            String filePath = path+'/'+fileName;
+            File dest = new File(filePath);
+            file.transferTo(dest);
+            if(nuseryService.addNusery(nusery) < 0){
+                return "添加失败";
+            }
+        }catch (Exception e){
+            e.printStackTrace();
             return "添加失败";
         }
         return "添加成功";
