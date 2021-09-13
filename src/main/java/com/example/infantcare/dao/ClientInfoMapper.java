d0a0a7c2262cf2727b6d05c3f69f85b76c5decf3
fawemv <3223818229@qq.com>
2021-09-13 15:56:30 +0800

    zengjia

--- src/main/java/com/example/infantcare/dao/ClientInfoMapper.java
+++ src/main/java/com/example/infantcare/dao/ClientInfoMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface ClientInfoMapper {
-    @Insert("INSERT INTO clientinfo(`id`, `name`, `inviter`, `phone`,`appointment`) VALUES #{id},#{name},#{inviter},#{phone},#{appointment}")
+    @Insert("INSERT INTO clientinfo(`id`,`name`, `inviter`, `phone`,`appointment`) VALUES (#{id},#{name},#{inviter},#{phone},#{appointment})")
     public void insertClient(ClientInfo info);
     @Select("SELECT name,inviter,phone,DATE_FORMAT(appointment,'%Y-%m-%d %H:%i:%S') appointment FROM clientinfo")
     public List<Map<String,Object>> selectClients();
