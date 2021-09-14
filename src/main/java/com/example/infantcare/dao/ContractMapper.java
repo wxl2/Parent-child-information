e6f7677bdf149234a5b1a5a935a3eadb4c8a5817
hutao <1067133459@qq.com>
2021-09-14 11:22:26 +0800

    添加合同金额

--- src/main/java/com/example/infantcare/dao/ContractMapper.java
+++ src/main/java/com/example/infantcare/dao/ContractMapper.java
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.Map;
 
 public interface ContractMapper {
-    @Insert("INSERT INTO contract(`id`,`name`,`phone`,`confinement`,`requiredate`,`requirement`,`nursery`) VALUES (#{id},#{name},#{phone},#{confinement},#{requireDate},#{requirement},#{nursery})")
+    @Insert("INSERT INTO contract(`id`,`name`,`phone`,`confinement`,`requiredate`,`requirement`,`nursery`,`conmoney`) VALUES (#{id},#{name},#{phone},#{confinement},#{requireDate},#{requirement},#{nursery},#{conMoney})")
     public void insertContract(Contract contract);
 
-    @Select("SELECT name,phone,DATE_FORMAT(confinement,'%Y-%m-%d') confinement,DATE_FORMAT(requiredate,'%Y-%m-%d') requiredate,requirement,nursery FROM contract")
+    @Select("SELECT name,phone,DATE_FORMAT(confinement,'%Y-%m-%d') confinement,DATE_FORMAT(requiredate,'%Y-%m-%d') requiredate,requirement,nursery,conmoney FROM contract")
     public List<Map<String,Object>> selectContract();
 }
