59f8de2c1b270786c33d3b54a064ec67ad7b9f21
fawemv <3223818229@qq.com>
2021-09-24 23:46:43 +0800

    set html add

--- src/main/java/com/example/infantcare/service/WorkingService.java
+++ src/main/java/com/example/infantcare/service/WorkingService.java
@@ -1,6 +1,7 @@
 package com.example.infantcare.service;
 
 import com.example.infantcare.dao.NuseryWorkingMapper;
+import com.example.infantcare.pojo.Nusery;
 import com.example.infantcare.pojo.NuseryLeusyre;
 import com.example.infantcare.pojo.NuseryWorking;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import java.util.Map;
 public class WorkingService {
     @Autowired
     NuseryWorkingMapper nuseryWorkingMapper;
+
     public int addWorkerRecord(NuseryWorking  nuseryWorking){
         try {
             nuseryWorkingMapper.insertNeseryWorking(nuseryWorking);;
@@ -36,6 +38,9 @@ public class WorkingService {
 
     public int addLeisureRecord(NuseryLeusyre nuseryLeusyre){
         try {
+            Nusery idNusery = getIdNusery(nuseryLeusyre.getId());
+            nuseryLeusyre.setImageurl(idNusery.getImageurl());
+            nuseryLeusyre.setLevel(idNusery.getLevel());
             nuseryWorkingMapper.insertNeseryLeysyre(nuseryLeusyre);;
             return 0;
         }catch (Exception e){
@@ -46,7 +51,8 @@ public class WorkingService {
 
     public List<Map<String,Object>> getLeisureRecords(){
         try {
-            List<Map<String,Object>> list = nuseryWorkingMapper.selecttNeseryLeysyre();
+            List<Map<String,Object>> list = null;
+            list = nuseryWorkingMapper.selecttNeseryLeysyre();
             return list;
         }catch (Exception e){
             e.printStackTrace();
@@ -58,6 +64,16 @@ public class WorkingService {
         try {
             List<String> list = nuseryWorkingMapper.selectWorkerTime(id);
             return list;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public Nusery getIdNusery(int id){
+        Nusery data = null;
+        try {
+             data = nuseryWorkingMapper.getIdNusery(id);
+            return data;
         }catch (Exception e){
             e.printStackTrace();
             return null;
