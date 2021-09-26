858852015fc1fee61850579e69f9fb37a204d752
hutao <1067133459@qq.com>
2021-09-26 12:55:13 +0800

    fix a small bug

--- src/main/java/com/example/infantcare/dao/StatementMapper.java
+++ src/main/java/com/example/infantcare/dao/StatementMapper.java
@@ -9,14 +9,11 @@ import java.util.List;
 import java.util.Map;
 
 public interface StatementMapper {
-    //  查询具有工作任务的育婴师
-    @Select("")
-    public List<Map<String,Object>> selectWorkNursery();
 
     //  添加订单
     @Insert("INSERT into sales(salesman,`name`,phone,requirement,money,`date`,`status`) values(#{salesman},#{name}" +
             ",#{phone},#{requirement},#{money},#{date},#{status})")
-    public  int insertOrder(Order o);
+    public int insertOrder(Order o);
 
     //展示订单
     @Select("select * from sales")
